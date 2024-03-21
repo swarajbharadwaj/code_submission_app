93e8a2fe13129e3a5d043a0eea213166965521af
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-21 23:17:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://code-submission-app-tyjp-ffe9ow95u-swaraj-kumars-projects.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://code-submission-app-tyjp-hppy7681c-swaraj-kumars-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
