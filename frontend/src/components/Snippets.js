1f033ead6f15c41e197d9d60b37e5297530df77a
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-21 22:48:44 +0530

    Update Snippets.js

--- frontend/src/components/Snippets.js
+++ frontend/src/components/Snippets.js
@@ -10,7 +10,7 @@ function Page2() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get("https://backend-swarajbharadwaj-swaraj-kumars-projects.vercel.app/snippets");
+        const response = await axios.get("https://code-submission-app.vercel.app/snippets");
         setEntries(response.data.reverse()); // Reverse the entries to display latest first
       } catch (error) {
         console.error('Error fetching data:', error);
