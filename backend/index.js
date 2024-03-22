0b6878ed9472e1f444f62b8fbdfb9d5597f6cb53
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-22 14:30:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const cors = require('cors');
-
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors()); // Set CORS headers globally
-
+app.use(cors());
 app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
   host: process.env.DB_host,
   user: process.env.DB_user,
   password: process.env.DB_password,
-  database: process.env.DB_database,
+  database:process.env.DB_database,
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -24,11 +23,13 @@ db.connect((err) => {
   console.log('Connected to MySQL database');
 });
 
+app.use(bodyParser.json());
+
 app.post('/submit', (req, res) => {
   const { username, codeLanguage, stdin, sourceCode, out } = req.body;
 
   const sql = 'INSERT INTO code_snippets (username, code_language, stdin, source_code, out_t) VALUES (?, ?, ?, ?, ?)';
-  db.query(sql, [username, codeLanguage, stdin, sourceCode, out], (err, result) => {
+  db.query(sql, [username, codeLanguage, stdin, sourceCode,out], (err, result) => {
     if (err) {
       res.status(500).json({ error: 'Failed to submit code snippet' });
       throw err;
