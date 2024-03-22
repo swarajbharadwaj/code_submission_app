2bcd458d802121d07bbd2be45ef561a7e5518aba
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-22 08:02:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://code-submission-app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
