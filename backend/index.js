eb5c12bb3040499a330f5f1007f8adee41d9a617
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-22 09:03:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const cors = require('cors');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+
 
 const db = mysql.createConnection({
   host: process.env.DB_host,
