212490d789c9fb1a5d401334f906efc73a4f3e7c
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-22 08:10:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,38 +4,37 @@ const mysql = require('mysql');
 const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(cors());
-
-const port = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
   host: process.env.DB_host,
   user: process.env.DB_user,
   password: process.env.DB_password,
-  database:process.env.DB_database,
+  database: process.env.DB_database,
 });
-
 
 db.connect((err) => {
   if (err) {
-    throw err;
+    console.error('Failed to connect to MySQL database:', err);
+  } else {
+    console.log('Connected to MySQL database');
   }
-  console.log('Connected to MySQL database');
 });
-
-app.use(bodyParser.json());
 
 app.post('/submit', (req, res) => {
   const { username, codeLanguage, stdin, sourceCode, out } = req.body;
 
   const sql = 'INSERT INTO code_snippets (username, code_language, stdin, source_code, out_t) VALUES (?, ?, ?, ?, ?)';
-  db.query(sql, [username, codeLanguage, stdin, sourceCode,out], (err, result) => {
+  db.query(sql, [username, codeLanguage, stdin, sourceCode, out], (err, result) => {
     if (err) {
+      console.error('Failed to submit code snippet:', err);
       res.status(500).json({ error: 'Failed to submit code snippet' });
-      throw err;
+    } else {
+      res.status(201).json({ message: 'Code snippet submitted successfully' });
     }
-    res.status(201).json({ message: 'Code snippet submitted successfully' });
   });
 });
 
@@ -43,10 +42,11 @@ app.get('/snippets', (req, res) => {
   const sql = 'SELECT username, code_language, stdin, LEFT(source_code, 100) AS source_code_short, out_t, timestamp FROM code_snippets';
   db.query(sql, (err, result) => {
     if (err) {
+      console.error('Failed to retrieve code snippets:', err);
       res.status(500).json({ error: 'Failed to retrieve code snippets' });
-      throw err;
+    } else {
+      res.status(200).json(result);
     }
-    res.status(200).json(result);
   });
 });
 
