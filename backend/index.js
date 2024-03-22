958eef1f185ae59d71bfa547fd5f02afc6307c0e
swarajbharadwaj <114842077+swarajbharadwaj@users.noreply.github.com>
2024-03-22 08:06:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+
 const port = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
